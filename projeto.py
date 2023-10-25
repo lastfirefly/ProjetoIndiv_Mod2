d6a9433df4cba62e9c254c2dfb2252479b4064b4
Beatriz Miranda <35241285+lastfirefly@users.noreply.github.com>
2023-10-25 16:14:31 -0300

    Update projeto.py
    Direitos autorais declarados.

--- projeto.py
+++ projeto.py
@@ -1,3 +1,19 @@
+# Copyright 2023 Beatriz Miranda
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#*********************_____Código do projeto_____*******************************
+
 #Importar as bibliotecas necessárias 
 import re  #Biblioteca própria do python para definir a expressão regular
 import pandas as pd #Biblioteca para fazer a exportação
